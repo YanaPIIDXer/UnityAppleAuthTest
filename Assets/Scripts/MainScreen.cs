62f6e90b70b1b7e84fa6e144b9c774b8ebf48929
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-12 11:31:45 +0900

    色々定義

---
+++ Assets/Scripts/MainScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// メインスクリーン
+/// </summary>
+public class MainScreen : MonoBehaviour
+{
+    /// <summary>
+    /// 結果表示用テキスト
+    /// </summary>
+    [SerializeField]
+    private Text resultText = null;
+
+    /// <summary>
+    /// 認証ボタン
+    /// </summary>
+    [SerializeField]
+    private Button authButton = null;
+}
---
+++ Assets/Scripts/Sequence.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// シーケンス
+/// </summary>
+public class Sequence : MonoBehaviour
+{
+    /// <summary>
+    /// メインスクリーン
+    /// </summary>
+    [SerializeField]
+    private MainScreen mainScreen = null;
+}
